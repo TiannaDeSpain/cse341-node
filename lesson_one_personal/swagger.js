885e23fe2bb649a2040d9fe91ae96bb9c9dee285
TiannaDeSapin <u663140950@byui.edu>
2023-04-28 12:37:55 -0700

    typo -http vs https

--- lesson_one_personal/swagger.js
+++ lesson_one_personal/swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'Temple API'
   },
   host: 'lesson-one-personal-tianna-despain.onrender.com',
-  schemes: ['http']
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
